84e5cd9f40ec6c08abe8afb177a14d9b5882e8f5
marinaliu <marinaliu121@outlook.com>
2017-12-04 14:42:32 +0800

    update

---
+++ 1.sh
@@ -0,0 +1,9 @@
+﻿git -c http.extraheader="AUTHORIZATION: bearer %SYSTEM_ACCESSTOKEN%" fetch
+git checkout -f master
+echo marina > 1.ps1
+git add .
+git commit -m change
+git tag 1.1.5
+git push origin master --tags
+1
+
